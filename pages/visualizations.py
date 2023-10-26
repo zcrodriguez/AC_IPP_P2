26a9ac35d4beaaf3caec40bafda3b0cdfbe68d1a
zcrodriguez <zc.rodriguez10@uniandes.edu.co>
2023-10-25 23:32:54 -0500

    Primera implementación exitosa de dcc.Store
    - Se implementó dcc.Store para guardar los datos de la base de datos y que se conserven entre páginas

--- pages/visualizations.py
+++ pages/visualizations.py
@@ -1,16 +1,18 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dcc, Input, Output, State, MATCH, ALL
+from dash.exceptions import PreventUpdate
 
 dash.register_page(__name__)
 
 layout = html.Div([
+    dcc.Store(id="session-store", data={}),
     html.H1('This is our Visualizations page'),
     html.Div([
         "Select a city: ",
         dcc.RadioItems(
             options=['New York City', 'Montreal', 'San Francisco'],
-            value='Montreal',
-            id='visualizations-input'
+            id='visualizations-input',
+            persistence=True  # Habilitar la persistencia para mantener el estado
         )
     ]),
     html.Br(),
@@ -18,9 +20,20 @@ layout = html.Div([
 ])
 
 
-@callback(
-    Output('visualizations-output', 'children'),
+@dash.callback(
+    Output('session-store', 'data'),
     Input('visualizations-input', 'value')
 )
-def update_city_selected(input_value):
-    return f'You selected: {input_value}'
+def update_session_data(selected_city):
+    if selected_city is None:
+        raise PreventUpdate
+    return {'selected_city': selected_city}
+
+
+@dash.callback(
+    Output('visualizations-output', 'children'),
+    Input('session-store', 'data')
+)
+def update_visualizations_output(data):
+    selected_city = data.get('selected_city', 'Montreal')
+    return f"You selected: {selected_city}"
